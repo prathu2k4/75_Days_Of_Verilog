193d1b700040dd317bfab359f2cf23945135f37d
Pratham Jain V S <prathucode@gmail.com>
2025-01-21 20:23:02 +0530

    Update binary_to_gray.v

--- Day11/binary_to_gray.v
+++ Day11/binary_to_gray.v
@@ -1,13 +1,11 @@
-// Binary-to-Gray Code Converter
 module binary_to_gray (
-    input wire [3:0] binary,  // 4-bit binary input
-    output wire [3:0] gray    // 4-bit Gray code output
+    input wire [3:0] binary,  
+    output wire [3:0] gray    
 );
 
-    // Conversion logic
-    assign gray[3] = binary[3];             // MSB remains the same
-    assign gray[2] = binary[3] ^ binary[2]; // XOR MSB with the next bit
-    assign gray[1] = binary[2] ^ binary[1]; // XOR next two bits
-    assign gray[0] = binary[1] ^ binary[0]; // XOR next two bits
+    assign gray[3] = binary[3];             
+    assign gray[2] = binary[3] ^ binary[2]; 
+    assign gray[1] = binary[2] ^ binary[1]; 
+    assign gray[0] = binary[1] ^ binary[0]; 
 
 endmodule
